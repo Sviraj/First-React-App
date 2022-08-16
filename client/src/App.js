74f0880f9a20c688bd1867ad8d6215bb10c468b8
sachinviraj <74185781+Sviraj@users.noreply.github.com>
2022-08-16 19:20:36 +0530

    Initial React

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {BrowserRouter} from "react-router-dom"; 
+import { Routes ,Route } from 'react-router-dom';
+import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Home from "./pages/Home";
+import AddEdit from "./pages/AddEdit";
+import View from "./pages/View";
+
+function App() {
+  return (
+
+    <BrowserRouter>
+      <div className="App">
+
+      <ToastContainer position="top-center"/>
+       <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route path='/addContact' element={<AddEdit/>} />
+          <Route path='/update/:id' element={<AddEdit/>} />
+          <Route path='/view/:id' element={<View/>} />
+       </Routes>
+ 
+    </div>
+    
+    
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,169 @@
+const express=require("express");
+const app=express();
+const bodyParser =require("body-parser");
+const mysql=require("mysql");
+const cors= require("cors");
+
+const db=mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password: "password",
+    database: "crud_contact",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/api/get",(req,res) => {
+    const sqlGet ="SELECT * FROM contact_db";
+    db.query(sqlGet, (error, result) =>{
+        res.send(result);
+    });
+});
+
+app.post("/api/post", (req, res) =>{
+    const {name, email, contact }=req.body;
+    const sqlInsert=
+    `INSERT INTO contact_db (name,email,contact) VALUES (?, ?, ?)`;
+    db.query(sqlInsert,[name,email,contact],(error,result) => {
+        if (error){
+            console.log(error);
+        }
+        res.send.apply(result);
+    });
+
+}); 
+
+app.delete("/api/remove/:id", (req, res) =>{
+    const { id }=req.params;
+    const sqlRemove=
+    `DELETE FROM contact_db WHERE id = ?`;
+    db.query(sqlRemove,[id],(error,result) => {
+        if (error){
+            console.log(error);
+        }
+    });
+
+});
+
+app.get("/api/get/:id", (req, res) => {
+    const { id } =req.params;
+    const sqlGet= "SELECT * FROM contact_db WHERE id= ?";
+    db.query(sqlGet, [id] , (error, result) =>{
+        if (error){
+            console.log(error);
+        }
+        res.send(result);
+       
+
+    });
+
+});
+
+// router.get('/:id',(req,res)=>{
+//     const sqlInsert=`SELECT * FROM contactdb WHERE id='${req.params.id}'`;
+//     db.query(sqlInsert,(error,result)=>{
+//         if(result!=null){
+//             res.json({state:true,data:result});
+//             return;
+//         }
+//         res.json({state:false,err:error});
+//     })
+// });
+
+
+app.put("/api/update/:id",(req,res) => {
+    const {id} =req.params;
+    const {name, email, contact}=req.body;
+    const sqlUpdate ="UPDATE contact_db SET name = ? ,email = ? ,contact = ? WHERE id = ?";
+    db.query(sqlUpdate, [name, email, contact, id], (error, result) => {
+        if (error){
+            console.log(error);
+        }
+        res.send(result);
+         
+    });
+});
+
+
+
+
+app.get("/", (req,res) =>{
+    // const sqlInsert= 
+    // "INSERT INTO contact_db (name,email,contact) VALUES ('Johrt','johrt@gmail.com',9999456)";
+    // db.query(sqlInsert, (err, result) =>{
+    //     console.log("error", error);
+    //     console.log("result", result);
+    //     res.send("Hello Express");
+    // });
+    
+});
+
+app.listen(5000 , () =>{
+    console.log("Server is running on port 5000");
+
+})
+
+// const router = require('express').Router();
+// const db = require('../server');
+
+// router.post('/add',(req,res)=>{
+//     const {name, email, contact} = req.body;
+//     const sqlInsert=`INSERT INTO contactdb (name, email, contact) VALUES ('${name}','${email}','${contact}')`;
+//     db.query(sqlInsert,(error,result)=>{
+//         if(result!=null){
+//             res.json({state:true,data:result});
+//             return;
+//         }
+//         res.json({state:false,err:error});
+//     })
+// });
+
+// router.get('/',(req,res)=>{
+//     const sqlInsert="SELECT * FROM contactdb";
+//     db.query(sqlInsert,(error,result)=>{
+//         if(result!=null){
+//             res.json({state:true,data:result});
+//             return;
+//         }
+//         res.json({state:false,err:error});
+//     })
+// });
+
+
+// router.get('/:id',(req,res)=>{
+//     const sqlInsert=`SELECT * FROM contactdb WHERE id='${req.params.id}'`;
+//     db.query(sqlInsert,(error,result)=>{
+//         if(result!=null){
+//             res.json({state:true,data:result});
+//             return;
+//         }
+//         res.json({state:false,err:error});
+//     })
+// });
+
+// router.put('/update/:id',(req,res)=>{
+//     const {name, email, contact} = req.body;
+//     const sqlInsert=`UPDATE contactdb SET name='${name}',email='${email}',contact='${contact}' WHERE id='${req.params.id}'`;
+//     db.query(sqlInsert,(error,result)=>{
+//         if(result!=null){
+//             res.json({state:true,data:result});
+//             return;
+//         }
+//         res.json({state:false,err:error});
+//     })
+// });
+
+// router.delete('/delete/:id',(req,res)=>{
+//     const sqlInsert=`DELETE FROM contactdb WHERE id='${req.params.id}'`;
+//     db.query(sqlInsert,(error,result)=>{
+//         if(result!=null){
+//             res.json({state:true,data:result});
+//             return;
+//         }
+//         res.json({state:false,err:error});
+//     })
+// });
+
+// module.exports = router;
